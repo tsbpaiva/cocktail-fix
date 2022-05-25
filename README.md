976f233841d8b17c46182c200a53665fac2c5416
Thais Paiva <tsbpaiva@gmail.com>
2022-05-25 16:04:00 +0200

    added more recipes to Menu

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Here's how it works:
 
 1. Just enter the liquor or ingredient that you have laying around your bar and the app will suggest cocktails you could make with it. 
 2. Choose one of the given options to see the full recipe. 
-3. If you're so fancy that you happen to have a liquor for which the app has no recipes, like cynar or malört, you'll have the option to choose again; just for tonight's drinking purposes, just pick something a bit more pedestrian - come on, if you have cynar on your spirits stash, we know you'll have a bottle of whiskey or vodka at your fingertips, and we have plenty of cool recipes for that.
+3. If you're so fancy that you happen to have a liquor for which the app has no recipes, like cynar or malört, you'll have the option to choose again; just for tonight's drinking purposes, pick something a bit more pedestrian - come on, if you have cynar ready at hand, you're bound to have a bottle of whiskey or vodka there too, and for these we do have plenty of cool recipes.
 4. You'll always get the chance to search for another recipe.
 5. When you're done, just tell the app you don't want to search for any other recipes, and our app will bid you farewell.
 
@@ -15,3 +15,5 @@ Made with entry-level knowledge of Python and best intentions only.
 Bottoms up!
 
 Thaís Paiva 
+
+Disclaimer: OBVIOUSLY, if you drink, don't drive. Theoretically you could code, but we advise against that too.
--- menu.py
+++ menu.py
@@ -10,5 +10,16 @@ class Menu:
 		{"name": "bloody mary", "ingredients": ["vodka", "tomato juice", "lemon juice", "celery stalk"]},
 		{"name": "screwdriver", "ingredients": ["vodka", "orange juice"]},
 		{"name": "mimosa", "ingredients": ["prosecco", "orange juice"]},
-		{"name": "irish coffee", "ingredients": ["cream", "coffee", "whiskey", "sugar", "nutmeg"]}
+		{"name": "irish coffee", "ingredients": ["cream", "coffee", "whiskey", "sugar", "nutmeg"]},
+		{"name": "margarita", "ingredients": ["tequila", "cointreau", "lime juice"]},
+		{"name": "tom collins", "ingredients": ["gin", "lemon juice", "soda water", "simple syrup"]},
+		{"name": "gin fizz", "ingredients": ["gin", "lemon", "simple syrup", "egg white", "soda water"]},
+		{"name": "moscow mule", "ingredients": ["vodka", "ginger beer", "lime juice"]},
+		{"name": "gin and tonic", "ingredients": ["gin", "tonic water"]},
+		{"name": "cosmopolitan", "ingredients": ["vodka", "cranberry juice", "cointreau", "lemon juice", "simple syrup"]},
+		{"name": "gimlet", "ingredients": ["gin", "lime juice", "simple syrup"]},
+		{"name": "aperol spritz", "ingredients": ["sparkling wine", "aperol", "soda water"]},
+		{"name": "sidecar", "ingredients": ["cognac", "cointreau", "lemon juice"]},
+		{"name": "boulevardieur", "ingredients": ["whiskey", "sweet vermouth", "campari"]},
+		{"name": "piña colada", "ingredients": ["rum", "cream of coconut", "pineapple juice", "lime juice"]},
 	]
